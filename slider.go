e45d4f2a5decf48a090d18e050a1f37eeb8d3d8e
reugn <reugpro@gmail.com>
2019-08-11 22:55:10 +0300

    use sweeper to shut down background processing

--- slider.go
+++ slider.go
@@ -1,53 +1,60 @@
 package equalizer
 
 import (
+	"runtime"
 	"sync"
 	"time"
+
+	"github.com/reugn/equalizer/internal"
 )
 
-// Slider is sliding window based rate limiter
+// Slider is a sliding window based rate limiter
 type Slider struct {
+	slider *slider
+}
+
+type slider struct {
 	sync.RWMutex
 	window   time.Duration
 	slide    time.Duration
 	capacity int
 	timeline []int64
 	issued   int
+	sweeper  *internal.Sweeper
 }
 
 // NewSlider returns new Slider instance
 func NewSlider(window time.Duration, slide time.Duration, capacity int) *Slider {
 	timelineArray := make([]int64, capacity)
-	slider := &Slider{
+	slider := &slider{
 		window:   window,
 		slide:    slide,
 		capacity: capacity,
 		timeline: timelineArray,
 		issued:   0,
 	}
-	go slider.init()
-	return slider
+	sl := &Slider{slider}
+	runSliderSweeper(slider, slide)
+	runtime.SetFinalizer(sl, stopSliderSweeper)
+	return sl
 }
 
-// init sliding window
-func (s *Slider) init() {
-	ticker := time.NewTicker(s.slide)
-	for range ticker.C {
-		s.Lock()
-		//clean outdated quotas and set counter
-		threshold := nowNano() - s.window.Nanoseconds()
-		s.timeline, s.issued = filter(s.timeline, threshold)
-		s.Unlock()
-	}
+// slide window
+func (s *slider) doSlide() {
+	s.Lock()
+	//clean outdated quotas and set counter
+	threshold := nowNano() - s.window.Nanoseconds()
+	s.timeline, s.issued = filter(s.timeline, threshold)
+	s.Unlock()
 }
 
 // Claim quota
 func (s *Slider) Claim() bool {
-	s.Lock()
-	defer s.Unlock()
-	if s.issued < s.capacity {
-		s.timeline[s.issued] = nowNano()
-		s.issued++
+	s.slider.Lock()
+	defer s.slider.Unlock()
+	if s.slider.issued < s.slider.capacity {
+		s.slider.timeline[s.slider.issued] = nowNano()
+		s.slider.issued++
 		return true
 	}
 	return false
@@ -67,4 +74,17 @@ func filter(arr []int64, threshold int64) ([]int64, int) {
 		}
 	}
 	return rt, i
+}
+
+func stopSliderSweeper(s *Slider) {
+	s.slider.sweeper.Stop <- struct{}{}
+}
+
+func runSliderSweeper(slider *slider, d time.Duration) {
+	s := &internal.Sweeper{
+		Interval: d,
+		Stop:     make(chan interface{}),
+	}
+	slider.sweeper = s
+	go s.Run(slider.doSlide)
 }
--- token_bucket.go
+++ token_bucket.go
@@ -1,41 +1,60 @@
 package equalizer
 
 import (
+	"runtime"
 	"sync/atomic"
 	"time"
+
+	"github.com/reugn/equalizer/internal"
 )
 
 // TokenBucket rate limiter
 type TokenBucket struct {
+	t *tokenBucket
+}
+
+type tokenBucket struct {
 	capacity       int32
 	refillInterval time.Duration
 	issued         int32
+	sweeper        *internal.Sweeper
 }
 
 // NewTokenBucket returns new TokenBucket instance
 func NewTokenBucket(capacity int32, refillInterval time.Duration) *TokenBucket {
-	tokenBucket := &TokenBucket{
+	tokenBucket := &tokenBucket{
 		capacity:       capacity,
 		refillInterval: refillInterval,
 		issued:         0,
 	}
-	go tokenBucket.init()
-	return tokenBucket
+	tb := &TokenBucket{tokenBucket}
+	runSweeper(tokenBucket, refillInterval)
+	runtime.SetFinalizer(tb, stopSweeper)
+	return tb
 }
 
-// init refill loop
-func (tb *TokenBucket) init() {
-	ticker := time.NewTicker(tb.refillInterval)
-	for range ticker.C {
-		atomic.StoreInt32(&tb.issued, 0)
-	}
+func (tb *tokenBucket) refill() {
+	atomic.StoreInt32(&tb.issued, 0)
 }
 
 // Claim quota
 func (tb *TokenBucket) Claim() bool {
-	if tb.issued < tb.capacity {
-		atomic.AddInt32(&tb.issued, 1)
+	if tb.t.issued < tb.t.capacity {
+		atomic.AddInt32(&tb.t.issued, 1)
 		return true
 	}
 	return false
+}
+
+func stopSweeper(t *TokenBucket) {
+	t.t.sweeper.Stop <- struct{}{}
+}
+
+func runSweeper(t *tokenBucket, d time.Duration) {
+	s := &internal.Sweeper{
+		Interval: d,
+		Stop:     make(chan interface{}),
+	}
+	t.sweeper = s
+	go s.Run(t.refill)
 }
